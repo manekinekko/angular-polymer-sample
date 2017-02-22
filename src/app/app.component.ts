2b0ee954fdbaa6a49836032b3eb4e121187eadda
Wassim CHEGHAM <github@wassimchegham.com>
2017-02-23 00:25:38 +0100

    feat: add a set of polymer component w/o any glue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  components = [
+    'iron-icons',
+    'paper-button',
+    'paper-checkbox',
+    'paper-card',
+    'paper-fab',
+    'paper-icon-button',
+    'paper-input',
+    'paper-listbox',
+    'paper-item',
+    'paper-menu',
+    'paper-slider',
+    'paper-spinner',
+    'paper-toast',
+    'paper-toggle-button'
+  ];
+
+  constructor() {
+    this.loadExternalComponents().then(_ => {
+      console.log('Polymer components loaded');
+    });
+  }
+
+  loadExternalComponents() {
+    return new Promise( (res, rej) => {
+      this.components.map((cmp) => {
+        const link = document.createElement('link');
+        link.rel = 'import';
+        link.href = `assets/bower_components/${cmp}/${cmp}.html`;
+        document.head.appendChild(link);
+        link.onerror = rej;
+        link.onload = (e) => {
+          res();
+        };
+      });
+    });
+  }
+
 }
